09d521c0202e03a0b60d2d24cdd08ebbd8651afa
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-03-11 23:33:31 +0530

    Time: 200 ms (10.32%), Space: 46.5 MB (97.13%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int len=nums.size();
+        for(int i=0;i<len-1;i++)
+        {
+            if(nums[i]==nums[i+1]) return 1;
+        }
+        return 0;
+    }
+};
