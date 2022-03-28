6307f1a2d588ae782a766dc478c7cc8c02c89145
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-03-29 01:47:01 +0530

    Time: 19 ms (46.41%), Space: 7.8 MB (46.48%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,63 +1,18 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int len=s.size(),count=0;
-        for(int i=0;i<len;i++)
-        {
-            if(s[i]=='I' && s[i+1]=='V')
-            {
-                count=count+4;
-                i++;
-            }else if(s[i]=='I' && s[i+1]=='X')
-            {
-                count=count+9;
-                i++;
-            }else if(s[i]=='I')
-            {
-                count=count+1;
-            }else if(s[i]=='X' && s[i+1]=='L')
-            {
-                count=count+40;
-                i++;
-            }else if(s[i]=='X' && s[i+1]=='C')
-            {
-                count=count+90;
-                i++;
-            }else if(s[i]=='X')
-            {
-                count=count+10;
-            }else if(s[i]=='C' && s[i+1]=='D')
-            {
-                count=count+400;
-                i++;
-            }else if(s[i]=='C' && s[i+1]=='M')
-            {
-                count=count+900;
-                i++;
-            }else if(s[i]=='C')
-            {
-                count=count+100;
-            }else if(s[i]=='V')
-            {
-                count=count+5;
-            }else if (s[i]=='X')
-            {
-                count=count+10;
-            }else if(s[i]=='L')
-            {
-                count=count+50;
-            }else if(s[i]=='C')
-            {
-                count=count+100;
-            }else if(s[i]=='D')
-            {
-                count=count+500;
-            }else if(s[i]=='M')
-            {
-                count=count+1000;
+        unordered_map<char,int> map={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        
+        int num = 0;
+        for (int i = 0; i < s.length() - 1; ++i) {
+            if (map[s[i]] < map[s[i + 1]]) {
+                num -= map[s[i]];
+            } else {
+                num += map[s[i]];
             }
-            
         }
-        return count;
+        num += map[s.back()];
+
+        return num;
     }
 };
