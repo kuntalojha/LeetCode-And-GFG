84c07478b429829b3e128880c01a830413a7b661
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-04-11 00:47:16 +0530

    Time: 3 ms (97.38%), Space: 10 MB (82.23%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -5,9 +5,9 @@ public:
        for(int i=0; i<arr.size(); i++){
            for(int j=i+1; j<arr.size(); j++){
                if(arr[i]==2*arr[j] || arr[j]==2*arr[i])
-                   return true;
+                   return 1;
            }
        }
-        return false;
+        return 0;
     }
 };
