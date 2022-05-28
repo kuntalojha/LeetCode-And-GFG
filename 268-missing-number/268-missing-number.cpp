5dfd9f4a209ce667aaa61db5dadaa085c725d4a5
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-05-29 02:37:50 +0530

    Time: 23 ms (68.75%), Space: 18 MB (20.58%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0; // sum of all numbers initially 0
+        int total = nums.size()*(nums.size() + 1)/2; // total is the sum of all numbers in the array
+        for (auto number : nums) { // iterate through the array
+            sum += number;
+        }
+        return total - sum;
+    }
+};
